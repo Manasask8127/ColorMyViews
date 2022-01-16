080734de5f9d574952c36c73d030d6cefb65f033
Manasask8127 <manasask8127@gmail.com>
2022-01-16 17:55:05 +0530

    added three buttons

--- app/src/main/java/com/example/manasask/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/manasask/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +19,9 @@ class MainActivity : AppCompatActivity() {
         val clickableViews : List<View> =
             listOf(findViewById(R.id.box_one_text),findViewById(R.id.box_two_text),
                 findViewById(R.id.box_three_text),findViewById(R.id.box_four_text),
-                findViewById(R.id.box_five_text),findViewById(R.id.constraint_layout))
+                findViewById(R.id.box_five_text),findViewById(R.id.constraint_layout),
+                findViewById(R.id.red_button),findViewById(R.id.blue_button),
+                findViewById(R.id.yellow_botton))
         for(item in clickableViews)
             item.setOnClickListener { makeColorChange(it) }
     }
@@ -29,8 +32,12 @@ class MainActivity : AppCompatActivity() {
             R.id.box_one_text -> View.setBackgroundColor(Color.MAGENTA)
             R.id.box_two_text -> View.setBackgroundColor(Color.GREEN)
             R.id.box_three_text -> View.setBackgroundColor(Color.YELLOW)
-            R.id.box_four_text -> View.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> View.setBackgroundColor(Color.DKGRAY)
             R.id.box_five_text -> View.setBackgroundColor(Color.WHITE)
+            R.id.red_button -> findViewById<TextView>(R.id.box_three_text).setBackgroundColor(Color.RED)
+            R.id.blue_button -> findViewById<TextView>(R.id.box_four_text).setBackgroundColor(Color.BLUE)
+            R.id.yellow_botton -> findViewById<TextView>(R.id.box_five_text).setBackgroundColor(Color.YELLOW)
+
             else ->
                 View.setBackgroundColor(Color.GRAY)
 
